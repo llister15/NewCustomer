aaee5dd4d397252e1b4e4152614e9d567a95b565
MrListe <rlister@wonkasoft.com>
2019-07-30 23:02:17 -0700

    first update

--- index.php
+++ index.php
@@ -1,1 +1,50 @@
 <?php
+/**
+ * Template for Z-Telco New Customers Sign up form.
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Z-Telco New Customer Form</title>
+<link rel="stylesheet" type="text/css" href="./NewCustomer/css/styles.css">
+</head>
+<body>
+<div class="page-wrap">
+	<header class="top-header">
+		<div class="logo">
+			<img class="responsive-img" src="" />
+		</div>
+		<div class="top-right-design">
+			<img class="responsive-img" src="" />
+		</div>
+	</header>
+	<address class="address-section">
+		<form class="inline-form">
+			<div class="left-form-side">
+				
+			</div>
+			<div class="right-form-side">
+				
+			</div>
+		</form>
+	</address>
+	<main>
+		<div class="product-table">
+			
+		</div>
+		<div class="services-area">
+			
+		</div>
+		<div class="add-ons-area">
+			
+		</div>
+	</main>
+	<footer>
+		<div class="footer-ads">
+			
+		</div>
+	</footer>
+</div>
+</body>
+</html>
